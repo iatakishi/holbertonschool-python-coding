6f4b602f0936e4b09ce04c508479a90b77ef141b
Idrak <iatakishi@gmail.com>
2026-01-20 23:35:00 +0400

    2 added

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines square class."""
+
+
+class Square:
+    """This class defines size of the square."""
+    def __init__(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        return self.__size ** 2
